734c535936db36474e75e1cdbe0fe3cff5173a93
tokcide <kanishk.jurel@gmail.com>
2023-03-10 18:59:48 +0530

    trying a deploy to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel()
+  adapter: netlify(),
 });
