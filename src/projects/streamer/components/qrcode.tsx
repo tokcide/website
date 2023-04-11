b190d620684fc4e0444d6c56a4bbf86958821bd4
tokcide <kanishk.jurel@gmail.com>
2023-04-11 07:10:25 +0530

    final push, changing camera after connection !impl

---
+++ src/projects/streamer/components/qrcode.tsx
@@ -0,0 +1,171 @@
+import { useStore } from "@nanostores/solid";
+import { createEffect, createResource, createSignal, onMount } from "solid-js";
+import { Show } from "solid-js/web";
+import { localConnectionId, peerConnection } from "../nano.store";
+import { targetConnectionId } from "../nano.store";
+
+// export const Scanner = () => {
+//   onMount(() => {
+//     function onScanSuccess(
+//       decodedText: string,
+//       decodedResult: Html5QrcodeResult
+//     ) {
+//       // handle the scanned code as you like, for example:
+//       alert("QR captured, you can stop scanning now");
+//       targetConnectionId.set(decodedText);
+//       console.log(`Code matched = ${decodedText}`, decodedResult);
+//     }
+
+//     let html5QrcodeScanner = new Html5QrcodeScanner(
+//       "reader",
+//       {
+//         fps: 10,
+//         qrbox: { width: 250, height: 450 },
+//       },
+//       false
+//     );
+//     html5QrcodeScanner.render(onScanSuccess, undefined);
+//   });
+//   return <div id="reader"></div>;
+// };
+
+export const Scanner = () => {
+  let video!: HTMLVideoElement, canvas!: HTMLCanvasElement;
+  const [clicked, setClicked] = createSignal(false);
+  const [stream$, setStream] = createSignal<MediaStream | null>(null);
+  const [device, setDevice] = createSignal<number>(-1);
+  const [cameras, { refetch }] = createResource(() =>
+    navigator.mediaDevices
+      .enumerateDevices()
+      .then((devices) =>
+        devices.filter((device) => device.kind === "videoinput")
+      )
+  );
+  onMount(() => {});
+  const flushStreams = () => {
+    stream$()
+      ?.getTracks()
+      .forEach((track) => track.stop());
+    video.srcObject = null;
+    video.load();
+  };
+  createEffect(() => {
+    if (clicked()) {
+      video.hidden = true;
+    } else {
+      video.srcObject = stream$();
+      video.load();
+    }
+  });
+  return (
+    <>
+      <Show
+        when={clicked()}
+        fallback={
+          <>
+            <div class="flex justify-center">
+              <video ref={video} autoplay playsinline></video>
+            </div>
+            <div
+              class="btn-group flex justify-center p-2"
+              role="group"
+              aria-label="QR video controls"
+            >
+              <button
+                class="btn btn-outline-primary"
+                onclick={() => {
+                  const cams = cameras();
+                  if (cams != null && cams!.length != 1) {
+                    const nextId =
+                      device() + 1 <= cams.length - 1 ? device() + 1 : 0;
+                    flushStreams();
+                    navigator.mediaDevices
+                      .getUserMedia({
+                        video: { deviceId: cams[nextId].deviceId },
+                      })
+                      .then((stream) => setStream(stream));
+                    setDevice(nextId);
+                  } else {
+                    navigator.mediaDevices
+                      .getUserMedia({
+                        video: true,
+                      })
+                      .then((stream) => setStream(stream));
+                  }
+                }}
+              >
+                Switch Camera
+              </button>
+              <button
+                class="btn btn-outline-primary"
+                onclick={() => {
+                  setClicked(true);
+                  canvas.width = video.videoWidth;
+                  canvas.height = video.videoHeight;
+                  canvas.getContext("2d")?.drawImage(video, 0, 0);
+                  flushStreams();
+                  canvas.toBlob((blob) => {
+                    const data = new FormData();
+                    if (blob) data.append("file", blob);
+                    fetch("/api/qrScanner", {
+                      body: data,
+                      method: "POST",
+                      headers: {
+                        "Access-Control-Allow-Origin": "*",
+                      },
+                    })
+                      .then((res) => res.json())
+                      .then((json) => {
+                        console.log(json);
+                        targetConnectionId.set(json[0].data ?? null);
+                      });
+                  }, "image/png");
+                }}
+              >
+                Take a picture
+              </button>
+            </div>
+          </>
+        }
+      >
+        <div class="flex justify-center">
+          <canvas ref={canvas}></canvas>
+          <div
+            class="btn-group-vertical"
+            role="group"
+            aria-label="Vertical button group"
+          >
+            <button
+              type="button"
+              class="btn btn-primary"
+              onclick={() => {
+                setClicked(false);
+              }}
+            >
+              Retake
+            </button>
+            <button type="button" class="btn btn-primary">
+              Proceed
+            </button>
+          </div>
+        </div>
+      </Show>
+    </>
+  );
+};
+
+export const Generate = () => {
+  const id = useStore(localConnectionId);
+
+  return (
+    <>
+      <Show when={id() != null} fallback={<div>Loading...</div>}>
+        <img
+          src={`https://api.qrserver.com/v1/create-qr-code/?data=${id()}&size=750x750`}
+          alt=""
+          title=""
+        />
+      </Show>
+    </>
+  );
+};
--- src/projects/streamer/nano.store.ts
+++ src/projects/streamer/nano.store.ts
@@ -4,3 +4,6 @@ import type Peer from "peerjs";
 export const peerConnection = atom<Peer | null>(null);
 export const localConnectionId = atom<string>("");
 export const targetConnectionId = atom<string>("");
+targetConnectionId.subscribe((v) => {
+  console.log("this is target", v);
+});
